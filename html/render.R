3a4a2c53fe05b94b711805f023471d757e81a46d
Rachel Steenson <rachel.steenson@glasgow.ac.uk>
2018-10-25 15:00:14 +0100

    Bug fixing

--- html/render.R
+++ html/render.R
@@ -1,5 +1,5 @@
-rmd_files <- list.files(path="rabies_web/html/",pattern="Rmd")
+rmd_files <- list.files(path="Serengeti/html/",pattern="Rmd")
 
 # print(rmd_files)
 
-sapply(rmd_files, function(x)rmarkdown::render(paste("rabies_web/html/",x,sep="")))
+sapply(rmd_files, function(x)rmarkdown::render(paste("Serengeti/html/",x,sep="")))
